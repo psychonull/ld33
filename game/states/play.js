84231a97410081d5c553d0a01ac8d4461aa55caa
gabrielcava <gabrielalejandrocava@gmail.com>
2015-08-22 19:42:21 -0300

    Food prefab

--- game/states/play.js
+++ game/states/play.js
@@ -1,46 +1,20 @@
 
   'use strict';
+  var Food = require('../prefabs/Food');
   function Play() {}
-
-  var cursors;
-  var movement = 75;
 
   Play.prototype = {
     create: function() {
       this.game.stage.backgroundColor = "#000000";
       this.game.physics.startSystem(Phaser.Physics.P2JS);
       this.game.physics.p2.defaultRestitution = 0.8;
-      this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
-      this.sprite.inputEnabled = true;
 
-      this.game.physics.p2.enable(this.sprite);
-
-      this.sprite.body.setZeroDamping();
-      this.sprite.body.fixedRotation = true;
-      this.sprite.body.velocity.x = 35;
-      this.sprite.body.velocity.y = 35;
-
-      cursors = this.game.input.keyboard.createCursorKeys();
+      var food = new Food(this.game, 100, 300);
+      this.game.add.existing(food);
+      
     },
     update: function() {
-      //this.sprite.body.setZeroVelocity();
-      if (cursors.left.isDown)
-      {
-        this.sprite.body.moveLeft(movement);
-      }
-      else if (cursors.right.isDown)
-      {
-        this.sprite.body.moveRight(movement);
-      }
 
-      if (cursors.up.isDown)
-      {
-        this.sprite.body.moveUp(movement);
-      }
-      else if (cursors.down.isDown)
-      {
-        this.sprite.body.moveDown(movement);
-      }
     },
     clickListener: function() {
       this.game.state.start('gameover');
