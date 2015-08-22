3a074b95ef4482da63e2e1f9d848163c47722257
Matias Arriola <matias.arriola@gmail.com>
2015-08-22 16:24:55 -0300

    Added fonts

--- game/states/menu.js
+++ game/states/menu.js
@@ -7,18 +7,16 @@ Menu.prototype = {
 
   },
   create: function() {
-    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
-    this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'yeoman');
-    this.sprite.anchor.setTo(0.5, 0.5);
+    this.game.stage.backgroundColor = "#A6947B";
 
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, '\'Allo, \'Allo!', style);
+    this.titleText =  this.game.add.bitmapText(this.game.world.centerX, 300, 'ka', 'MONSTER MADNESS', 64);
+    this.titleText.tint = 0x4D0C15;
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400, 'Click anywhere to play "Click The Yeoman Logo"', { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.instructionsText =  this.game.add.bitmapText(this.game.world.centerX, 400, 'arcade', 'Press enter to start!', 48);
+    this.instructionsText.tint = 0x4D0C15;
     this.instructionsText.anchor.setTo(0.5, 0.5);
 
-    this.sprite.angle = -20;
-    this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
--- game/states/play.js
+++ game/states/play.js
@@ -3,10 +3,12 @@
   function Play() {}
   Play.prototype = {
     create: function() {
+      this.game.stage.backgroundColor = "#000000";
+
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
       this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
       this.sprite.inputEnabled = true;
-      
+
       this.game.physics.arcade.enable(this.sprite);
       this.sprite.body.collideWorldBounds = true;
       this.sprite.body.bounce.setTo(1,1);
@@ -22,5 +24,5 @@
       this.game.state.start('gameover');
     }
   };
-  
+
   module.exports = Play;
