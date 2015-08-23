a2c6b986d73948b417fc461e4172a76309069c59
Matias Arriola <matias.arriola@gmail.com>
2015-08-23 20:27:53 -0300

    menu

--- game/states/menu.js
+++ game/states/menu.js
@@ -1,5 +1,6 @@
 
 'use strict';
+
 function Menu() {}
 
 Menu.prototype = {
@@ -7,16 +8,15 @@ Menu.prototype = {
 
   },
   create: function() {
-    this.game.stage.backgroundColor = "#A6947B";
+    this.game.stage.backgroundColor = "#153030";
 
-    this.titleText =  this.game.add.bitmapText(this.game.world.centerX, 300, 'ka', 'MONSTER MADNESS', 64);
-    this.titleText.tint = 0x4D0C15;
+    this.titleText =  this.game.add.bitmapText(this.game.world.centerX, 300, 'ka', 'WASTE MADNESS', 64);
+    this.titleText.tint = 0x34f3ff;
     this.titleText.anchor.setTo(0.5, 0.5);
 
-    this.instructionsText =  this.game.add.bitmapText(this.game.world.centerX, 400, 'arcade', 'Press enter to start!', 48);
-    this.instructionsText.tint = 0x4D0C15;
+    this.instructionsText =  this.game.add.bitmapText(this.game.world.centerX, 400, 'p2', '<<Press any key to continue>>', 22);
+    this.instructionsText.tint = 0x34f3ff;
     this.instructionsText.anchor.setTo(0.5, 0.5);
-
   },
   update: function() {
     if(this.game.input.activePointer.justPressed()) {
