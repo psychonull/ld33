b7ed3552063ed758b000620f8c8cfc723671d3fc
dnColo <diego.n.colo@hotmail.com>
2015-08-22 23:44:30 -0300

    Removed commented code

--- game/prefabs/monster.js
+++ game/prefabs/monster.js
@@ -65,44 +65,10 @@ Monster.prototype.update = function() {
     {
       //this.body.moveDown(movement);
     }
-  
-
-   /*var targetAngle = this.game.math.angleBetween(
-        this.x, this.y,
-        this.game.input.activePointer.x, this.game.input.activePointer.y
-    );
-
-    // Gradually (this.TURN_RATE) aim the missile towards the target angle
-    if (this.rotation !== targetAngle) {
-        // Calculate difference between the current angle and targetAngle
-        var delta = targetAngle - this.rotation; 
-
-        // Keep it in range from -180 to 180 to make the most efficient turns.
-        if (delta > Math.PI) delta -= Math.PI * 2;
-        if (delta < -Math.PI) delta += Math.PI * 2;
-
-        if (delta > 0) {
-            // Turn clockwise
-            this.angle += this.TURN_RATE;
-        } else {
-            // Turn counter-clockwise
-            this.angle -= this.TURN_RATE;
-        }
-
-        // Just set angle to target angle if they are close
-        if (Math.abs(delta) < this.game.math.degToRad(this.TURN_RATE)) {
-            this.rotation = targetAngle;
-        }
-    }*/
-
-    // Calculate velocity vector based on this.rotation and this.SPEED
+ 
     this.body.velocity.x = Math.cos(this.rotation) * this.SPEED;
     this.body.velocity.y = Math.sin(this.rotation) * this.SPEED;
-    }    
-
-
-    //this.body.width = this.getBounds().width;
-    //this.body.height = this.getBounds().height;
+    }
   
 };
 
