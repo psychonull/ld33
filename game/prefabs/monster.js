cf928d42069f8935bd4da47ddfeae5aca3e6b5ac
Marcelo <marcelogvallejos@gmail.com>
2015-08-23 19:54:44 -0300

    changing settings for scale

--- game/prefabs/monster.js
+++ game/prefabs/monster.js
@@ -116,4 +116,9 @@ Monster.prototype.jump = function() {
   this.jumpFX.play();
 };
 
+Monster.prototype.increaseSize = function() {
+  this.scale.x += settings.growth_scale;
+  this.scale.y += settings.growth_scale;
+};
+
 module.exports = Monster;
--- game/settings.js
+++ game/settings.js
@@ -3,5 +3,6 @@ module.exports = {
     width: 1000,
   	height: 1920
   },
-  water_level: 650
+  water_level: 650,
+  growth_scale: 0.05
 };
